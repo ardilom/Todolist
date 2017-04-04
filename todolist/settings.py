48c3b206a3cbfb99d1cfce4d4b3e185aa913aa68
Ardilom <fardila120@gmail.com>
2017-04-04 03:54:26 +0000

    settings.py update

--- todolist/settings.py
+++ todolist/settings.py
@@ -75,14 +75,25 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-}
-DATABASES['default'] =  dj_database_url.config()
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'test_db',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
+    }
+else:
+    DATABASES = {
+    #    'default': {
+    #        'ENGINE': 'django.db.backends.sqlite3',
+    #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #    }
+    }
+    DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation
