2bbe6db51e089b96b51ba2d4d7326d90175eacd9
Ardilom <fardila120@gmail.com>
2017-04-04 03:32:01 +0000

    settings.py update

--- todolist/settings.py
+++ todolist/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 }
-DATABASES['default'] =  dj_database_url.config(default=DATABASE_URL)
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation
