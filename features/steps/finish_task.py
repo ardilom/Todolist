26be47b02a6ff7fea732faca8486afa7941d21a6
Ardilom <fardila120@gmail.com>
2017-05-11 02:43:14 +0000

    #8 behave for travis and heroku added

--- features/steps/finish_task.py
+++ features/steps/finish_task.py
@@ -1,13 +1,18 @@
 from behave import given, when, then
 from selenium import webdriver
-from datetime import datetime
+import os
 
 @given(u'I go to "/list_tasks" to finish a task')
 def step_impl(context):
     #deberia ir en enviroment.py, pero nos falla
     context.browser = webdriver.PhantomJS()
     context.browser.implicitly_wait(1)
-    context.server_url = 'https://todolist-ardilom.c9users.io/list_task'
+    if 'TRAVIS' in os.environ:
+        context.server_url = 'localhost/list_task'
+    elif 'on_heroku' in os.environ:
+        context.server_url = 'https://todolist-ardilla.herokuapp.com/list_task'
+    else:
+        context.server_url = 'https://todolist-ardilom.c9users.io/list_task'
     
     br = context.browser
     br.get(context.base_url + '/list_tasks')
